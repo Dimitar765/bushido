34c32499b592cc3fbfa7ad607783cfe814f54e5b
Dimitar765 <112801140+Dimitar765@users.noreply.github.com>
2024-01-27 19:46:08 +0100

    import fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 
 import './App.css'
 import CardContainer from './components/card-container/CardContainer'
-import Card from './components/card/Card'
+// import Card from './components/card/Card'
 import Hero from './components/hero/Hero'
 import Navbar from './components/navbar/Navbar'
 
