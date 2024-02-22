1d394df94f62240bdee29061015eb1b8fb0051d7
Dimitar765 <112801140+Dimitar765@users.noreply.github.com>
2024-02-22 12:00:35 +0100

    data

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,20 +1,24 @@
 // import React from 'react'
-import './Hero.css'
+import "./Hero.css";
 
-function Hero() {
+function Hero(): JSX.Element {
   return (
     <>
-    <div className="heroContainer">
+      <div className="heroContainer">
         <div className="imgContainer">
-          <img className='heroImg' src="Outlook-Image.jpg" alt="" />
+          <img className="heroImg" src="Outlook-Image.jpg" alt="" />
         </div>
         <div className="headingContainer">
-        <h1 className='heroHeading'>Karateto kako nacin na zivot</h1>
-        <h3 className='heroSubHeading'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, assumenda?</h3>
+          <h1 className="heroHeading">Karateto kako nacin na zivot</h1>
+          <h3 className="heroSubHeading">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo,
+            assumenda?
+          </h3>
         </div>
-    </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
+
